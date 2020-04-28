c47b64405c0fa11e362964556e45c56be5566c32
Mizukis <45371365+Mizukis@users.noreply.github.com>
2020-04-28 14:23:09 +0200

    Add files via upload

---
+++ Lesson_1_4.php
@@ -0,0 +1,15 @@
+<?php
+$h1 = 'HI-TECH DESIGN';
+$title = 'Art Of Coding';
+$date = date ( 'd.F Y H:i');
+?>
+
+<h1><?= $h1?></h1>
+
+<p><?= $title?></p>
+
+<span>Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua
+enim minim estudiat veniam siad venumus dolore</span>
+
+<div> Today <?= $date?>  </div>
+
